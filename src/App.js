8b486715e5a80dd9b459a0ce4798185d96f0eed8
Shazwi Suwandi <supershazwi@gmail.com>
2019-11-04 22:55:00 +0800

    anchor

--- src/App.js
+++ src/App.js
@@ -21,10 +21,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="https://google.com"
-          >
+          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="/">
             BTFL
           </a>
         </nav>
